df908d0e13de3d274e2de02d747643c22791e0e1
NHAlexPham <123584989+NHAlexPham@users.noreply.github.com>
2023-03-15 16:07:39 -0400

    Changement de Spring Boot
    Code fait pour MongoDB,
    Le code ne marche toujours pas à l'école

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
